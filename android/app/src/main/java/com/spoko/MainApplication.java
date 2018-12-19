41f7c175a487ab92818e8611a1e466a0df90cceb
Lukasz Nawrocki <lukasz@aurity.co>
2018-12-19 02:45:39 +0100

    Maps work

--- android/app/src/main/java/com/spoko/MainApplication.java
+++ android/app/src/main/java/com/spoko/MainApplication.java
@@ -3,7 +3,6 @@ package com.spoko;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage()
       );
