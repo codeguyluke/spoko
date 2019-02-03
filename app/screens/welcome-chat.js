cfbe9cdfea173174b7c3da3cbbca0446d7afd2ff
Lukasz Nawrocki <lukasz@aurity.co>
2019-02-03 15:10:25 +0100

    Google are fucking idiots

--- app/screens/welcome-chat.js
+++ app/screens/welcome-chat.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { KeyboardAvoidingView, AsyncStorage, StyleSheet } from 'react-native'
-import firebase from 'react-native-firebase'
+import { KeyboardAvoidingView, StyleSheet } from 'react-native'
 import { FAB, TextInput } from 'react-native-paper'
 import { Loader, Container, JoChat } from '../components'
 
@@ -28,85 +27,6 @@ export default class WelcomeChat extends Component {
     loading: false,
   }
 
-  async componentDidMount() {
-    this.unsubscribe = firebase.links().onLink(async url => {
-      if (firebase.auth().isSignInWithEmailLink(url)) {
-        this.setState({ loading: true })
-        try {
-          const email = await AsyncStorage.getItem('emailForSignIn')
-          await firebase.auth().signInWithEmailLink(email, url)
-        } catch (error) {
-          this.setState(prevState => ({
-            loading: false,
-            messages: [
-              ...prevState.messages,
-              {
-                type: 'error',
-                text: error.message,
-              },
-            ],
-          }))
-        }
-      }
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  handleSendLink = async () => {
-    const { email } = this.state
-    this.setState(prevState => ({
-      loading: true,
-      messages: [
-        ...prevState.messages,
-        { author: 'me', text: `Please send login link to ${email}.` },
-      ],
-    }))
-
-    try {
-      const actionCodeSettings = {
-        url: 'https://spoko-dev.firebaseapp.com',
-        handleCodeInApp: true,
-        iOS: {
-          bundleId: 'com.codice.spoko',
-        },
-        android: {
-          packageName: 'com.codice.spoko',
-          installApp: true,
-          minimumVersion: '12',
-        },
-      }
-
-      await firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-      await AsyncStorage.setItem('emailForSignIn', email)
-      this.setState(prevState => ({
-        loading: false,
-        messages: [
-          ...prevState.messages,
-          {
-            text: `We sent login link to ${email}!`,
-          },
-          {
-            text: 'Please open the email on your phone and click the link in order to sign in.',
-          },
-        ],
-      }))
-    } catch (error) {
-      this.setState(prevState => ({
-        loading: false,
-        messages: [
-          ...prevState.messages,
-          {
-            text: error.message,
-            type: 'error',
-          },
-        ],
-      }))
-    }
-  }
-
   render() {
     const { messages, loading, email } = this.state
 
@@ -125,7 +45,7 @@ export default class WelcomeChat extends Component {
               keyboardType="email-address"
               style={styles.distance}
             />
-            <FAB label="Send me login link" onPress={this.handleSendLink} style={styles.distance} />
+            <FAB label="Send me login link" onPress={() => {}} style={styles.distance} />
           </KeyboardAvoidingView>
         )}
       </Container>
